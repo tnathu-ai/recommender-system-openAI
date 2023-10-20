5c183663c6f87bf6360a86d6ff4ca4c83f34beef
tnathu-ai <72063833+tnathu-ai@users.noreply.github.com>
2023-10-20 13:06:37 +1100

    clean

--- rec-sys/embeddings.py
+++ rec-sys/embeddings.py
@@ -12,7 +12,7 @@ def get_embeddings(texts: list[str], model="text-embedding-ada-002") -> list[lis
     return [item["embedding"] for item in openai.Embedding.create(input=texts, model=model)["data"]]
 
 
-def train_and_evaluate_embeddings_model(df, columns_for_unique_pairs=['title'], batch_size=100):
+def train_and_evaluate_embeddings_model(df, columns_for_unique_pairs=['title'], batch_size=100, RANDOM_STATE=RANDOM_STATE, TEST_SIZE=TEST_SIZE, N_ESTIMATORS=N_ESTIMATORS):
     # Assuming the user column is named 'reviewerID'
 
     embeddings = {}
@@ -43,10 +43,10 @@ def train_and_evaluate_embeddings_model(df, columns_for_unique_pairs=['title'], 
 
     # Splitting the dataset
     X_train_openai, X_test_openai, y_train, y_test = train_test_split(
-        X_openai, df['rating'], test_size=0.2, random_state=42)
+        X_openai, df['rating'], test_size=TEST_SIZE, random_state=RANDOM_STATE)
 
     # Train RandomForest on OpenAI embeddings
-    rfr_openai = RandomForestRegressor(n_estimators=100)
+    rfr_openai = RandomForestRegressor(n_estimators=N_ESTIMATORS)
     rfr_openai.fit(X_train_openai, y_train)
 
     # Predict and evaluate
