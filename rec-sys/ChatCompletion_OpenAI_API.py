caa33fe278a5fe6dfd3e37a0653ce18360b57009
tnathu-ai <72063833+tnathu-ai@users.noreply.github.com>
2023-12-27 13:29:23 +0700

    updated output

--- rec-sys/ChatCompletion_OpenAI_API.py
+++ rec-sys/ChatCompletion_OpenAI_API.py
@@ -376,3 +376,71 @@ def predict_ratings_with_collaborative_filtering_and_save(data, pcc_matrix,
 
     return results_df
 
+
+
+def rerun_failed_CF_fewshot_predictions(data, pcc_matrix, 
+                                        save_path, 
+                                        user_column_name, 
+                                        movie_column_name, 
+                                        movie_id_column, 
+                                        rating_column_name, 
+                                        num_ratings_per_user, 
+                                        num_main_user_ratings, 
+                                        num_similar_users, 
+                                        new_path, 
+                                        rerun_indices):
+    original_data = pd.read_csv(save_path)
+    original_data.columns = ['user_id', 'item_id', 'title', 'actual_rating', 'predicted_rating']
+    
+    unique_users = data[user_column_name].unique()
+    user_id_to_index = {user_id: idx for idx, user_id in enumerate(unique_users)}
+
+    for index in rerun_indices:
+        user_id = original_data.at[index, 'user_id']
+        item_id = original_data.at[index, 'item_id']
+        user_idx = user_id_to_index[user_id]
+
+        print(f"Rerunning prediction for User ID: {user_id}, Item ID: {item_id} (Index: {index})")
+
+        main_user_data = data[data[user_column_name] == user_id]
+        main_user_ratings = main_user_data.sample(n=num_main_user_ratings)
+        main_user_ratings_str = '\n'.join([
+            f"* Title: {row[movie_column_name]}, Rating: {row[rating_column_name]} stars"
+            for _, row in main_user_ratings.iterrows()
+        ])
+
+        similar_users_idx = np.argsort(-pcc_matrix[user_idx])[:num_similar_users + 1]
+        similar_users_idx = similar_users_idx[similar_users_idx != user_idx][:num_similar_users]
+
+        similar_users_ratings = ""
+        for idx in similar_users_idx:
+            similar_user_id = unique_users[idx]
+            similar_user_data = data[data[user_column_name] == similar_user_id]
+            historical_ratings = similar_user_data.sample(n=num_ratings_per_user)
+
+            for _, row in historical_ratings.iterrows():
+                rating_info = f"* Title: {row[movie_column_name]}, Rating: {row[rating_column_name]} stars"
+                similar_users_ratings += rating_info + "\n"
+
+        # Select the specific movie that failed prediction
+        failed_movie_row = data[(data[user_column_name] == user_id) & (data[movie_id_column] == item_id)].iloc[0]
+        failed_movie_title = failed_movie_row[movie_column_name]
+
+        combined_text = f"Title: {failed_movie_title}"
+        
+        # Call to your prediction function
+        predicted_rating = predict_rating_combined_ChatCompletion(
+            combined_text, 
+            approach="CF", 
+            similar_users_ratings=similar_users_ratings,
+            rating_history=main_user_ratings_str
+        )
+
+        # Update the predicted rating in the original data
+        original_data.at[index, 'predicted_rating'] = predicted_rating
+
+        print(f"Updated prediction for User ID: {user_id}, Item ID: {item_id}: {predicted_rating}")
+
+    # Save the updated predictions to a new file
+    original_data.to_csv(new_path, index=False)
+    print(f"Updated predictions saved to {new_path}")
